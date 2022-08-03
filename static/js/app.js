89c9618df3bb0ff4bc50906fbba2a10ab8fcfe09
Stephanie <user@USERs-MacBook-Pro.local>
2022-08-02 22:39:30 -0700

    finished tableData code on app.js

--- static/js/app.js
+++ static/js/app.js
@@ -19,3 +19,25 @@ data.forEach((dataRow) => {
         }
     );
 });
+
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // check to see of a date was enterd and filter the data using that date
+    if (date) {
+        // apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row.datetime === date);
+    }
+
+    // rebuild the table using the filtered data
+    //@ note: if no date was entered, then filteredData will just be the original tableData
+    buildTable(filteredData);   
+}
+
+// attach an event to listen for the form button 
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
